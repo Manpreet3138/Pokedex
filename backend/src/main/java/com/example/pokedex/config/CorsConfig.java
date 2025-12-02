dfc4d4e8e163dd45022715f8855c6e5e1d15ff7b
Manpreet3138 <manpreetsingh3138@gmail.com>
2025-12-02 16:16:16 +0530

    config file added

---
+++ backend/src/main/java/com/example/pokedex/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.example.pokedex.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                // allow your frontend origin
+                .allowedOrigins("https://manpreet3138.github.io")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
